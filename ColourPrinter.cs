4bd6b333d14c65a72cbfcbea1f648aef6251a8b1
Ghostling225 <wojcikadam742@gmail.com>
2022-11-27 15:19:02 +0100

    Add method documentation to ColourPrinter

--- ColourPrinter.cs
+++ ColourPrinter.cs
@@ -1,10 +1,20 @@
 namespace Consolidate;
 
 public static class ColourPrinter {
+	/// <summary>
+	/// Prints a string of text in the specified color and appends a newline character to it.
+	/// </summary>
+	/// <param name="message">The message to display</param>
+	/// <param name="color">The color to use</param>
 	public static void WriteLine(string message, ConsoleColor color) {
 		Write($"{message}\n", color);
 	}
 
+	/// <summary>
+	/// Prints a string of text in the specified color.
+	/// </summary>
+	/// <param name="message">The message to display</param>
+	/// <param name="color">The color to use</param>
 	public static void Write(string message, ConsoleColor color) {
 		var previousColor = Console.ForegroundColor;
 		Console.ForegroundColor = color;
